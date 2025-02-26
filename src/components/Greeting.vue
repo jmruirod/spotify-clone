aa088ab10d67fc59d3c29685b9a0fb1e89b5d8e5
José Manuel Ruiz Rodríguez <jmruirod@gmail.com>
2025-02-26 15:26:13 +0100

    Add view transitions and vue greeting component

---
+++ src/components/Greeting.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const currentTime = new Date();
+const currentHour = currentTime.getHours();
+
+let greeting = "";
+
+if (currentHour < 12) {
+  greeting = "Buenos días";
+} else if (currentHour < 18) {
+  greeting = "Buenas tardes";
+} else {
+  greeting = "Buenas noches";
+}
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold">{{ greeting }}</h1>
+</template>
+
+<style scoped></style>
